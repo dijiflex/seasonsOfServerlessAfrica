eda991661000d69411e692fe8cec24c9e9cf497e
dijiflex <dijiflex@gmail.com>
2021-01-03 15:27:32 +0300

    Added parcel bundler

---
+++ js/app.js
@@ -0,0 +1,64 @@
+
+import axios from 'axios';
+
+var map;
+
+async function addMarkers(){
+    try {
+        const res = await axios.get('https://africarecipefinder.azurewebsites.net/api/recipes');
+        console.log(res.status);
+        console.log(res.data);
+    } catch (error) {
+        console.log(error.response.data.message);
+    }
+}
+
+function initMap() {
+     map = new atlas.Map('myMap', {
+        center: [36.817223 ,-1.286389],
+        zoom: 12,
+        language: 'en-US',
+        authOptions: {
+            authType: 'subscriptionKey',
+            subscriptionKey: '5Og9orBviOwprjzLr6PsmIJ50WzziX5ZYKnceLGnWLQ'
+        }
+    });
+
+    //Wait until the map resources are ready.
+    map.events.add('ready', function () {
+        //Add zoom controls to bottom right of map.
+        map.controls.add(new atlas.control.ZoomControl(), {
+            position: 'bottom-right'
+        });
+
+        //Add map style control in top left corner of map.
+        map.controls.add(new atlas.control.StyleControl(), {
+            position: 'top-left'
+        });
+    });
+
+    
+    
+}
+const searchForm = document
+
+export const getSearch = ()=> {
+  const searchForm = document.getElementById('searchForm');
+  searchForm.addEventListener('submit', e =>{
+    e.preventDefault()
+   
+    map.setCamera({
+    center: [-111.0225, 35.0272],
+    zoom: 12
+   
+}, 6000);
+  })
+
+}
+
+
+
+
+
+
+window.onload = initMap();
--- js/index.js
+++ js/index.js
@@ -1,54 +1,6 @@
 
-var map;
+import '@babel/polyfill';
 
-function initMap() {
-     map = new atlas.Map('myMap', {
-        center: [36.817223 ,-1.286389],
-        zoom: 12,
-        language: 'en-US',
-        authOptions: {
-            authType: 'subscriptionKey',
-            subscriptionKey: '5Og9orBviOwprjzLr6PsmIJ50WzziX5ZYKnceLGnWLQ'
-        }
-    });
+import { getSearch } from "./app";
 
-    //Wait until the map resources are ready.
-    map.events.add('ready', function () {
-        //Add zoom controls to bottom right of map.
-        map.controls.add(new atlas.control.ZoomControl(), {
-            position: 'bottom-right'
-        });
-
-        //Add map style control in top left corner of map.
-        map.controls.add(new atlas.control.StyleControl(), {
-            position: 'top-left'
-        });
-    });
-
-    
-    
-}
-
-const searchForm = document
-
-function getSearch() {
-  const searchForm = document.getElementById('searchForm');
-  searchForm.addEventListener('submit', e =>{
-    e.preventDefault()
-   
-    map.setCamera({
-    center: [-111.0225, 35.0272],
-    zoom: 12
-   
-}, 6000);
-  })
-
-}
-
-getSearch()
-
-
-
-
-
-window.onload = initMap();
+getSearch();
